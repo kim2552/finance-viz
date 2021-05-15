8acd265dea4c901e6e4e2af6fac36ce68501d2af
joohoonkim <davidkim2552@gmail.com>
2021-05-15 12:41:47 -0400

    plot investment chart

--- dollar_cost_avg.py
+++ dollar_cost_avg.py
@@ -5,6 +5,8 @@ import yfinance as yf
 import yahoofinancials
 from yahoofinancials import YahooFinancials
 import datetime
+import numpy as np
+import matplotlib.pyplot as plt
 
 '''
 FinancePy
@@ -22,6 +24,9 @@ start_day = 1
 end_year = 2019
 end_month = 10
 end_day = 1
+
+colors =['blue','red','green','orange']
+counter = 0
 
 def getInfoBiWeekly(ticker,start_dt,end_dt):
     yahoo_financials = YahooFinancials(ticker)
@@ -55,7 +60,8 @@ def getInfoBiWeekly(ticker,start_dt,end_dt):
     print("\tMy return percentage = ",'%.2f'%(((final-dca)/dca)*100),"%")
     print("\tMy return rate = $",'%.2f'%(my_investment*((final-dca)/dca)))
 
-def getInfoMonthly(ticker,start_dt,end_dt):
+def getInfoMonthly(ticker,start_dt,end_dt,ax):
+    global counter
     yahoo_financials = YahooFinancials(ticker)
     my_investment = 0
 
@@ -69,12 +75,28 @@ def getInfoMonthly(ticker,start_dt,end_dt):
     count=0
     total=0
     final=0
+
+    date_list = []
+    total_list = []
+    dates = np.array(date_list)
+    totals = np.array(total_list)
+    print(data)
+    total_shares = 0
     for i in data[ticker].get('prices'):
         my_investment += 500
         count+=1
         avg=( (i.get('high')+i.get('low')) / 2)
         total+=avg
         final=avg
+        num_shares =500/avg
+        total_shares+=num_shares
+        current_equity = total_shares*avg
+
+        dates = np.append(dates, i.get('date'))
+        totals = np.append(totals, current_equity)
+
+    ax.plot(dates, totals, color=colors[counter])
+    counter +=1
 
     dca = total/count
     print("\tCurrent price = $",final)
@@ -84,18 +106,26 @@ def getInfoMonthly(ticker,start_dt,end_dt):
     print("\tMy return percentage = ",'%.2f'%(((final-dca)/dca)*100),"%")
     print("\tMy return rate = $",'%.2f'%(my_investment*((final-dca)/dca)))
 
-t = input("Enter ticker symbol: ")
-start_date = input("Enter start date (Y-m-d): ")
-end_date = input("Enter end date (Y-m-d): ")
+fig, ax = plt.subplots()
+while(True):
+    t = input("Enter ticker symbol: ")
+    start_date = input("Enter start date (Y-m-d): ")
+    end_date = input("Enter end date (Y-m-d): ")
 
-for i in range(1):
-    if(not(start_date) or not(end_date)):
-        ds = datetime.date(start_year,start_month,start_day).strftime("%Y-%m-%d")
-        de = datetime.date(end_year,start_month,start_day).strftime("%Y-%m-%d")
-    else:
-        ds = start_date
-        de = end_date
-    if(not(t)):
-        t = "VFV.TO"
-    getInfoBiWeekly(t,ds,de)
-    getInfoMonthly(t,ds,de)
+    for i in range(1):
+        if(not(start_date) or not(end_date)):
+            ds = datetime.date(start_year,start_month,start_day).strftime("%Y-%m-%d")
+            de = datetime.date(end_year,start_month,start_day).strftime("%Y-%m-%d")
+        else:
+            ds = start_date
+            de = end_date
+        if(not(t)):
+            t = "VFV.TO"
+        # getInfoBiWeekly(t,ds,de)
+        getInfoMonthly(t,ds,de,ax)
+
+    ax.set(xlabel='Time (s)', ylabel='Total ($)',
+       title='Investment Total by Time')
+    ax.grid()
+
+    fig.savefig("investment.png")
